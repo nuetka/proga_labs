433e1094647cc1bad81ad15c3c895202a88569f1
nuetka <126546509+nuetka@users.noreply.github.com>
2023-12-08 15:35:13 +0700

    lab7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,85 +1,42 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws CloneNotSupportedException {
+    public static void main(String[] args) {
 
-        System.out.println("Производный класс:");
+        Track tr = new Track("DS", "Rock", 202);
+        ExtendedTrack etr = new ExtendedTrack("EDS", "KPOP", 300,  "Jgz");
+        Track tr1 = new Track("Mill", "Bambam", 20);
+        ExtendedTrack etr1 = new ExtendedTrack("Akka", "Wow", 303, "jjj");
 
-        ExtendedTrack ext_track = new ExtendedTrack("Song1", "Pop", 3, "Эта песня посвящена Кире"); // отрицательная продолжительность
+        List<Track> tracks = new ArrayList<>();
+        tracks.add(tr);
+        tracks.add(etr);
+        tracks.add(tr1);
+        tracks.add(etr1);
 
-        ext_track.printToConsole();
-        System.out.println("-------------includeBaseInfo=true--------------");
-        ext_track.printToConsole(true);
-        System.out.println("---------includeBaseInfo=false---------------");
-        ext_track.printToConsole(false);
+        System.out.println("Сортировка треков по названию:");
+        tracks.sort((a, b) -> a.getName().compareTo(b.getName()));
+        printTracksToConsole(tracks);
 
-        System.out.println("\nИспользование переопределённого метода toString");
-        String trackInfo = ext_track.toString();
-        System.out.println(trackInfo);
+        System.out.println("\nСортировка треков по продолжительности:");
+        tracks.sort((a, b) -> Integer.compare(a.getDuration(), b.getDuration()));
+        printTracksToConsole(tracks);
 
-        System.out.println("\nИспользование интерфйеса ChangeVolumne");
-        ext_track.volumeUp();
-        Playlist playlist = new Playlist("PlaylistName");
-        playlist.volumeUp();
-
-        System.out.println("\nИспользование абстракотного класса MediaItem");
-        ext_track.play();
-        Movie movie = new Movie("MovieName", 1000, "АюАю", 3033);
-        movie.play();
-
-
-        Movie originalMovie = new Movie("Title", 3000, "John", 2022);
-
-        Movie clonedMovie = (Movie) originalMovie.clone();
-
-        System.out.println("\nМелкое клонирование");
-        System.out.println("OriginalMovie: " + "Title:" + originalMovie.getName() + ";Duration:" + originalMovie.getDuration() + ";Director" + originalMovie.getDirector() + ";Release" + originalMovie.getReleaseYear());
-        System.out.println("ClonedMovie: " + "Title:" + clonedMovie.getName() + ";Duration:" + clonedMovie.getDuration() + ";Director" + clonedMovie.getDirector() + ";Release" + clonedMovie.getReleaseYear());
-
-
-        Track track1 = new Track("Song1", "Pop1", 3);
-        Track track2 = new Track("Song2", "Pop2", 4);
-        List<Track> someTracks = new ArrayList<>(Arrays.asList(track1, track2));
-
-        System.out.println("\nГлубокое клонирование");
-        Album originalAlbum = new Album("Name", 2022, "Artist", someTracks);
-        Album clonedAlbum = (Album) originalAlbum.clone();
-        System.out.println("\nOriginalAlbum");
-        originalAlbum.printToConsole();
-
-        System.out.println("\nClonedAlbum");
-        clonedAlbum.printToConsole();
-
-
-        System.out.println("\nиспользование шаблона \"Singleton\"");
-        // Получаем единственный экземпляр библиотеки
-        Library library = Library.getInstance();
-
-        // Добавляем альбомы и плейлисты
-        Album album1 = new Album("Album 1", 2022, "Artist 1", new ArrayList<>());
-        Album album2 = new Album("Album 2", 2022, "Artist 2", new ArrayList<>());
-
-        library.addAlbum(album1);
-        library.addAlbum(album2);
-
-        Playlist playlist1 = new Playlist("Playlist 1", new ArrayList<>());
-        Playlist playlist2 = new Playlist("Playlist 2", new ArrayList<>());
-
-        library.addPlaylist(playlist1);
-        library.addPlaylist(playlist2);
-
-        // Используем библиотеку
-        List<Album> albums = library.getAlbums();
-        List<Playlist> playlists = library.getPlaylists();
-
-        for (Album album : albums) {
-            System.out.println("Album: " + album.getName());
+        System.out.println("\nПоиск треков по жанру Rock:");
+        String genreToSearch = "Rock";
+        List<Track> foundTracks = new ArrayList<>();
+        for (Track track : tracks) {
+            if (track.getGenre().equals(genreToSearch)) {
+                foundTracks.add(track);
+            }
         }
+        printTracksToConsole(foundTracks);
+    }
 
-        for (Playlist pl : playlists) {
-            System.out.println("Playlist: " + pl.getName());
+    private static void printTracksToConsole(List<Track> tracks) {
+        for (Track track : tracks) {
+            track.printToConsole();
         }
     }
 
